529fc94430195df60d31ec997ac82014c3f491a3
sanskar khare <sanskarkhare@sanskars-MacBook-Air.local>
2022-07-11 14:58:50 +0530

    tests

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 const Loan = artifacts.require("Loan");
 
-module.exports =async function (deployer,accounts) {
+module.exports =async function (deployer,network, accounts) {
+  console.log(accounts[1])
   await deployer.deploy(Loan, 1000, 10, 100, accounts[1], accounts[2]);
 };
